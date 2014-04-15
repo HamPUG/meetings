38128f047b8afc2a89b43e5dfa945e68cb563506
U-tess\cjb60 <cjb60@tess.(none)>
2014-04-15 13:51:01 +1200

    Moved files to files folder, updated get.py and added README.md

--- 2014-04-14/files/get.py
+++ 2014-04-14/files/get.py
@@ -1,6 +1,8 @@
 import urllib2
 import re
 import glob
+import time
+import os
 
 def geturl(url,agent=None):
     headers = { 'User-Agent': agent }
@@ -12,6 +14,9 @@ def getbetween(body, start, end):
     strs = re.findall( re.escape(start) + "(.*?)" + re.escape(end), body, re.DOTALL)
     return strs
 
+ONLINE = True
+OUTPUT_DIR = "saved"
+SLEEP_TIME = 0.5 # in seconds
 
 prefix = "http://www.lyricsfreak.com/m/metallica"
 page = geturl("http://www.lyricsfreak.com/m/metallica/")
@@ -21,18 +26,19 @@ for link in links:
     if prefix in link:
         new_links.append(link)
 
-"""
-for i,v in enumerate(new_links):
-    page = geturl(v)
-    f = open("saved/" + str(i) + ".html", "wb")
-    f.write(page)
-    f.flush()
-    f.close()
+if ONLINE:
+	for i,v in enumerate(new_links):
+		page = geturl(v)
+		# see if the "saved" folder exists first
+		if not os.path.exists(OUTPUT_DIR):
+			os.makedirs(OUTPUT_DIR)
+		f = open(OUTPUT_DIR + os.sep + str(i) + ".html", "wb")
+		f.write(page)
+		f.flush()
+		f.close()
+		time.sleep(SLEEP_TIME)
 
-    print v
-"""
-
-files = glob.glob("saved/*.html")
+files = glob.glob(OUTPUT_DIR + os.sep + "*.html")
 for filename in files:
     body = open(filename).read()
     lyrics = getbetween(body, "<div id='content_h' class='dn'>", "</div>")[0]
