494ab1f297b4cc4a7ffe30c80921068e5c140714
Ian Stewart <irsbugs@hotmail.com>
2023-01-05 21:23:43 +1300

    Add files via upload

--- 2022/2022-12-12/demo_progs/switch.py
+++ 2022/2022-12-12/demo_progs/switch.py
@@ -1,36 +1,23 @@
 # switch.py
-# Simple switch that turns on/off the pin25 led.
+# Switch that turns on/off the GP25 led.
 # Not interupt driven. Updates every second.
-#
+
 import time
 from machine import Pin
 
-# switch_1 connects from pin14 to +3.3V rail.
-switch_1 = Pin(14, Pin.IN,  Pin.PULL_DOWN)
+# switch_1, when closed, connects pin14 to the ground rail.
+# Pin.PULL_UP keeps the pin at 3.15V when swtich is open.
+switch_1 = Pin(14, Pin.IN,  Pin.PULL_UP)
 
-led = Pin(25, Pin.OUT)
+# Led on the Pico module, GP25.
+led_1 = Pin(25, Pin.OUT)
 
 while True:
     #print(dir(switch_1))
-    print("switch_1.value():", switch_1.value())  # 1 when switch is on
+    print("switch_1.value():", switch_1.value())  # 1 when switch is off
     if switch_1.value() == 1:
-        led.value(1)
+        led_1.value(1)
     else:
-        led.value(0)
+        led_1.value(0)
     time.sleep(1)
-    
-"""
->>> %Run -c $EDITOR_CONTENT
 
-switch_1.value(): 0
-switch_1.value(): 0
-switch_1.value(): 0
-switch_1.value(): 0
-switch_1.value(): 1
-switch_1.value(): 1
-switch_1.value(): 0
-switch_1.value(): 0
-switch_1.value(): 1
-switch_1.value(): 1
-switch_1.value(): 0    
-"""
