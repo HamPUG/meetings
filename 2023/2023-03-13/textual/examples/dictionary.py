d1bdbc370979ed3e1c06184e3fd7234bdd6b0316
Peter Reutemann <fracpete@gmail.com>
2023-03-12 22:15:17 +1300

    added rquirements.txt; added basic textual stuff/examples

---
+++ 2023/2023-03-13/textual/examples/dictionary.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+import asyncio
+
+try:
+    import httpx
+except ImportError:
+    raise ImportError("Please install httpx with 'pip install httpx' ")
+
+
+from textual.app import App, ComposeResult
+from textual.containers import Content
+from textual.widgets import Input, Markdown
+
+
+class DictionaryApp(App):
+    """Searches ab dictionary API as-you-type."""
+
+    CSS_PATH = "dictionary.css"
+
+    def compose(self) -> ComposeResult:
+        yield Input(placeholder="Search for a word")
+        with Content(id="results-container"):
+            yield Markdown(id="results")
+
+    def on_mount(self) -> None:
+        """Called when app starts."""
+        # Give the input focus, so we can start typing straight away
+        self.query_one(Input).focus()
+
+    async def on_input_changed(self, message: Input.Changed) -> None:
+        """A coroutine to handle a text changed message."""
+        if message.value:
+            # Look up the word in the background
+            asyncio.create_task(self.lookup_word(message.value))
+        else:
+            # Clear the results
+            await self.query_one("#results", Markdown).update("")
+
+    async def lookup_word(self, word: str) -> None:
+        """Looks up a word."""
+        url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            try:
+                results = response.json()
+            except Exception:
+                self.query_one("#results", Static).update(response.text)
+                return
+
+        if word == self.query_one(Input).value:
+            markdown = self.make_word_markdown(results)
+            await self.query_one("#results", Markdown).update(markdown)
+
+    def make_word_markdown(self, results: object) -> str:
+        """Convert the results in to markdown."""
+        lines = []
+        if isinstance(results, dict):
+            lines.append(f"# {results['title']}")
+            lines.append(results["message"])
+        elif isinstance(results, list):
+            for result in results:
+                lines.append(f"# {result['word']}")
+                lines.append("")
+                for meaning in result.get("meanings", []):
+                    lines.append(f"_{meaning['partOfSpeech']}_")
+                    lines.append("")
+                    for definition in meaning.get("definitions", []):
+                        lines.append(f" - {definition['definition']}")
+                    lines.append("---")
+
+        return "\n".join(lines)
+
+
+if __name__ == "__main__":
+    app = DictionaryApp()
+    app.run()
---
+++ 2023/2023-03-13/textual/examples/json_tree.py
@@ -0,0 +1,93 @@
+import json
+from pathlib import Path
+
+from rich.text import Text
+
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Tree
+from textual.widgets.tree import TreeNode
+
+
+class TreeApp(App):
+
+    BINDINGS = [
+        ("a", "add", "Add node"),
+        ("c", "clear", "Clear"),
+        ("t", "toggle_root", "Toggle root"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Tree("Root")
+
+    @classmethod
+    def add_json(cls, node: TreeNode, json_data: object) -> None:
+        """Adds JSON data to a node.
+
+        Args:
+            node (TreeNode): A Tree node.
+            json_data (object): An object decoded from JSON.
+        """
+
+        from rich.highlighter import ReprHighlighter
+
+        highlighter = ReprHighlighter()
+
+        def add_node(name: str, node: TreeNode, data: object) -> None:
+            """Adds a node to the tree.
+
+            Args:
+                name (str): Name of the node.
+                node (TreeNode): Parent node.
+                data (object): Data associated with the node.
+            """
+            if isinstance(data, dict):
+                node._label = Text(f"{{}} {name}")
+                for key, value in data.items():
+                    new_node = node.add("")
+                    add_node(key, new_node, value)
+            elif isinstance(data, list):
+                node._label = Text(f"[] {name}")
+                for index, value in enumerate(data):
+                    new_node = node.add("")
+                    add_node(str(index), new_node, value)
+            else:
+                node._allow_expand = False
+                if name:
+                    label = Text.assemble(
+                        Text.from_markup(f"[b]{name}[/b]="), highlighter(repr(data))
+                    )
+                else:
+                    label = Text(repr(data))
+                node._label = label
+
+        add_node("JSON", node, json_data)
+
+    def on_mount(self) -> None:
+        """Load some JSON when the app starts."""
+        file_path = Path(__file__).parent / "food.json"
+        with open(file_path) as data_file:
+            self.json_data = json.load(data_file)
+
+    def action_add(self) -> None:
+        """Add a node to the tree."""
+        tree = self.query_one(Tree)
+        json_node = tree.root.add("JSON")
+        self.add_json(json_node, self.json_data)
+        tree.root.expand()
+
+    def action_clear(self) -> None:
+        """Clear the tree (remove all nodes)."""
+        tree = self.query_one(Tree)
+        tree.clear()
+
+    def action_toggle_root(self) -> None:
+        """Toggle the root node."""
+        tree = self.query_one(Tree)
+        tree.show_root = not tree.show_root
+
+
+if __name__ == "__main__":
+    app = TreeApp()
+    app.run()
